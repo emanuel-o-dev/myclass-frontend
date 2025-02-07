f0e005c4a16c39c3d60c455f2eabf3432adbe584
emanuel.o.dev <101219040+emanuel-o-dev@users.noreply.github.com>
2025-02-07 02:25:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import AlunoList from "./components/AlunoList";
+import AlunoList from "./components/AlunosList";
 import AlunoForm from "./components/AlunoForm";
 
 function App() {
